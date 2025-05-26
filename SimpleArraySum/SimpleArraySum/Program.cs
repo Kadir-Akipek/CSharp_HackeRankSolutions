7c31fbcd567699a48d5bbf99e4a29ab52d5b6282
Kadir Akipek <akipekkadir23@gmail.com>
2025-05-26 09:28:55 +0300

    .

--- SimpleArraySum/SimpleArraySum/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿public static int simpleArraySum(List<int> ar)
-{
-	int sum = 0;
-	foreach (int number in ar)
-	{
-		sum += number;
-	}
-	return sum;
-}
--- SolveMeFirst/SolveMeFirst/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿static int solveMeFirst(int a, int b)
-{
-	// Hint: Type return a+b; below  
-	return a + b;
-}
